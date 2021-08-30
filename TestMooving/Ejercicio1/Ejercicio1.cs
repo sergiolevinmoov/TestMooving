a0a4adf443395d50463127b320fc8a43a7bb06b5
sergio david levin <sergio.levin@moovingtech.com>
2021-08-30 09:32:12 -0300

    Add project files.

---
+++ TestMooving/Ejercicio1/Ejercicio1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMooving.Ejercicio1
+{
+    public class Ejercicio1
+        {
+            private static string result ;
+
+            public static void DiAlgo()
+            {
+                DiAlgoAsync();
+                Console.WriteLine("Result :" + result);
+            }
+
+            static async Task<string> DiAlgoAsync()
+            {
+                await Task.Delay(5);
+                result = "Hola!";
+                return "Algo";
+            }
+        }
+}
---
+++ TestMooving/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMooving
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
